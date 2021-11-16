e6fad7102a6a33ac2b321bc4866c2263eeeda0fc
Dawid Chara <Dawid.Chara@sabre.com>
2021-11-16 22:51:03 +0000

    Infrastructure + Deployment - restored iam policy... it was needed

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -24,6 +24,23 @@ resource "google_cloud_run_service" "run_service" {
   depends_on = [google_project_service.run_api]
 }
 
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.run_service.location
+  project     = google_cloud_run_service.run_service.project
+  service     = google_cloud_run_service.run_service.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
 output "service_url" {
   value = google_cloud_run_service.run_service.status[0].url
 }
